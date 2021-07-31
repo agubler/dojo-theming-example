aefc2813efb871712a08b6745240ee38d34a5ec1
Anthony Gubler <agubler@sitepen.com>
2021-07-31 11:55:14 +0100

    example

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,38 @@
-import { tsx, create } from '@dojo/framework/core/vdom';
-import theme from '@dojo/framework/core/middleware/theme';
-import Outlet from '@dojo/framework/routing/Outlet';
-import dojo from '@dojo/widgets/theme/dojo';
+import { tsx, create } from "@dojo/framework/core/vdom";
+import theme from "@dojo/framework/core/middleware/theme";
+import dojo from "@dojo/widgets/theme/dojo";
+import material from "@dojo/widgets/theme/material";
 
-import Menu from './widgets/Menu';
-import Home from './widgets/Home';
-import About from './widgets/About';
-import Profile from './widgets/Profile';
+import Switch from "@dojo/widgets/switch";
+import Stack, { Spacer } from "@dojo/widgets/stack";
 
-import * as css from './App.m.css';
+import * as css from "./App.m.css";
 
 const factory = create({ theme });
 
-export default factory(function App({ middleware: { theme } }) {
-	if (!theme.get()) {
-		theme.set(dojo);
-	}
-	return (
-		<div classes={[css.root]}>
-			<Menu />
-			<Outlet id="main">
-				{{
-					home: <Home />,
-					about: <About />,
-					profile: <Profile username="Dojo User" />
-				}}
-			</Outlet>
-		</div>
-	);
+export default factory(function App() {
+  return (
+    <div classes={[css.root]}>
+      <Stack direction="vertical" spacing="large">
+        <Spacer />
+        <Switch
+          theme={{
+            theme: dojo,
+            variant: { name: "default", value: dojo.variants.default },
+          }}
+          value={true}
+          onValue={() => {}}
+        />
+        <Switch
+          theme={{
+            theme: material,
+            variant: { name: "default", value: material.variants.default },
+          }}
+          value={true}
+          onValue={() => {}}
+        />
+        <Spacer />
+      </Stack>
+    </div>
+  );
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import renderer, { tsx } from '@dojo/framework/core/vdom';
-import Registry from '@dojo/framework/core/Registry';
-import { registerRouterInjector } from '@dojo/framework/routing/RouterInjector';
+import renderer, { tsx } from "@dojo/framework/core/vdom";
+import Registry from "@dojo/framework/core/Registry";
+import { registerRouterInjector } from "@dojo/framework/routing/RouterInjector";
 
-import routes from './routes';
-import App from './App';
+import routes from "./routes";
+import App from "./App";
 
 const registry = new Registry();
 registerRouterInjector(routes, registry);
